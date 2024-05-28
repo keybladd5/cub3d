da230650a8850008557031fe6f516943160af900
Ales de Toledo Delgado <alesdetoledodelgado@MacBook-Pro-de-Ales.local>
2024-05-28 13:38:44 +0200

    -.-

--- Makefile
+++ Makefile
@@ -18,7 +18,7 @@ FLAGS	=	-Wall -Wextra -Werror -MMD -g -fsanitize=address
 
 MLX_F	=	-Iinclude -lglfw -framework Cocoa -framework OpenGL -framework IOKit
 
-SRC		=	main.c draw.c render.c movement.c
+SRC		=	main.c
 
 OBJ		=	$(SRC:.c=.o)
 
--- main.c
+++ main.c
@@ -12,63 +12,8 @@
 
 #include "cub3d.h"
 
-
-void ft_free_map(char **map)
-{
-	int i;
-
-	i = 0;
-	while(map[i])
-	{
-		printf("%p\n", map[i]);
-		free(map[i]);
-		i++;
-	}
-	printf("%p\n", map);
-	free(map);
-}
-
-void ft_game_loop(void *param)
-{
-	t_data *d = param;
-	mlx_delete_image(d->mlx, d->image);
-	d->image = mlx_new_image(d->mlx, 0, 0);
-	ft_movement_hook(d);
-	ft_cast_rays(d);
-	mlx_image_to_window(d->mlx, d->image, 0, 0);
-}
-
 int main(void)
 {
-	t_data	d;
-
-	d.map = ft_calloc(8, sizeof(char *)); // init the map
-	d.map[0] = ft_strdup("11111111"); //fill the map
-	d.map[1] = ft_strdup("10000001");
-	d.map[2] = ft_strdup("10000001");
-	d.map[3] = ft_strdup("10000001");
-	d.map[4] = ft_strdup("10000001");
-	d.map[5] = ft_strdup("11111111");
-	d.map[6] = NULL;
-	//he añadido una serie de datos relevantes al player, que necesito pasar por referencia
-	//sin mallocs
-	d.data_player.advance = 0;
-	d.data_player.turn_on =  0;
-	d.data_player.angle_rotation = M_PI;
-	d.data_player.speed_advance = 3.0;
-	d.data_player.speed_advance = 3.0;
-	d.data_player.speed_turn_on = 1.0 * (M_PI / 180.0);
-	d.data_player.x = 0.0;
-	d.data_player.y = 0.0;
-	d.data_player.fov_radians = (FOV * M_PI) / 180;
-
-	
-	//nuevas variables añadidas para los rayos
-	d.data_player.west = false;
-	d.data_player.south = false;
-	d.cast_rays.x_intercept = 0;
-	d.cast_rays.y_intercept = 0;
-
 	//carga la ventana
 	d.mlx = mlx_init(WIDTH, HEIGHT, "cub3d", true);
 
